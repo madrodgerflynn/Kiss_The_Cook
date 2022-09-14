45743fa3eb475c34e92b9116f48d17ea9b38c670
relero90 <relero90@gmail.com>
2022-09-14 16:50:07 -0600

    built out server.js; added seed to scripts in package.json

---
+++ Server.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+// creates the sessions table for us automatically
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const routes = require("./Controller");
+const sequelize = require("./Config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
