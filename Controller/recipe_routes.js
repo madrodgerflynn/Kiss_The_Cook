c08c4cc6deb27af4fdb1e2d9e7ffacbbd3ba26fb
madrodgerflynn <madrodgerflynn@gmail.com>
2022-09-15 10:16:17 -0600

    intial commit not many changes

--- Controller/recipe_routes.js
+++ Controller/recipe_routes.js
@@ -1,2 +1,27 @@
 const router = require("express").Router();
-const recipe = require("../Models/users");
+const { recipes, users } = require("../Models");
+
+router.get("/", async (req, res) => {
+  try {
+    const dbRecipeData = await recipes.findAll({
+      include: [
+        {
+          model: Painting,
+          attributes: ["filename", "description"],
+        },
+      ],
+    });
+
+    const allRecipes = dbGalleryData.map((gallery) =>
+      gallery.get({ plain: true })
+    );
+    // Send over the 'loggedIn' session variable to the 'homepage' template
+    res.render("homepage", {
+      galleries,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
