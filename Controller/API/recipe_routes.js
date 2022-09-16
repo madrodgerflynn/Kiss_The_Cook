07a32e22004241432640cdd1cd60a0b8f0442b6b
madrodgerflynn <madrodgerflynn@gmail.com>
2022-09-16 11:16:36 -0600

    fixed getAllRecipes

--- Controller/API/recipe_routes.js
+++ Controller/API/recipe_routes.js
@@ -3,7 +3,7 @@ const { Recipe, User } = require("../../Models/index");
 const chalk = require("chalk");
 
 // route --> /api/recipes
-router.get("/recipes", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const dbRecipeData = await Recipe.findAll({
       include: [
@@ -19,12 +19,38 @@ router.get("/recipes", async (req, res) => {
       recipe.get({ plain: true })
     );
     console.log(chalk.blue(allRecipes)); //or this
-
-    res.render("all-recipes", { allRecipes });
+    res.status(200).json(allRecipes);
+    // res.render("all-recipes", { allRecipes });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
+//finding one recipe
+router.get("/recipes/:id", async (req, res) => {
+  try {
+    const dbRecipeData = await Recipe.findByPk(req.params.id, {
+      include: [
+        {
+          model: Recipe,
+          attributes: [
+            "id",
+            "title",
+            "image",
+            "ingredients",
+            "recipe_steps",
+            "user_id",
+          ],
+        },
+      ],
+    });
 
+    const recipe = dbRecipeData.get({ plain: true });
+    // Send over the 'loggedIn' session variable to the 'gallery' template
+    res.render("recipes", { recipe, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
