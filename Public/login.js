8ed2c51d8840baf58f562409b1082077683be0bc
Michaela Varberg <michaelavarberg@gmail.com>
2022-09-16 11:17:22 -0600

    created one-recipe handlebars

--- Public/login.js
+++ Public/login.js
@@ -14,7 +14,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace("/recipes");
+      document.location.replace("/api/recipes");
     } else {
       feedback.textContent = "Failed to login. Please try again!";
       //   then refresh page?
