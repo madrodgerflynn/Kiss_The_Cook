171bf74d6a7b253d2348be10f3b3d582df344ec0
Michaela Varberg <michaelavarberg@gmail.com>
2022-09-20 11:26:29 -0600

    added more seeds and fixed bug

--- Seeds/recipeData.js
+++ Seeds/recipeData.js
@@ -52,6 +52,58 @@ const recipeData = [
     ]),
     user_id: "1",
   },
+  {
+    title: "Pumpkin Chocolate Chip Bread",
+    image:
+      "https://docs.google.com/drawings/d/e/2PACX-1vRsibX6YGJVRiGutVoyIZhfMoNprJVcnU82HVeBFmWOT5LPqEgDcc7vmwQVpbkaIBYBV9Gdrz-iQz5h/pub?w=593&h=422",
+    ingredients: JSON.stringify([
+      "2 cups flour",
+      "1 cup brown sugar",
+      "1 tblspn baking powder",
+      "1 tsp salt",
+      "1/4 tsp salt",
+      "1/4 tsp baking soda",
+      "1/4 tsp nutmeg",
+      "1/8 tsp ginger or ground cloves",
+      "1 cup canned pumpkin",
+      "1/2 cup milk",
+      "2 eggs",
+      "1/3 cup butter",
+      "1/2 bag of chocolate chips",
+    ]),
+    recipe_steps: JSON.stringify([
+      "In a large bowl, combine 1 cup of flour, brown sugar, baking powder, nutmeg and ginger.",
+      "Add pumpkin, milk, eggs and butter.",
+      "Beat on low speed until blended, then high speed for 2 minutes.",
+      "Add remaining flour and beat well.",
+      "Add chocolate chips.",
+      "Bake at 350 for 60-65 minutes.",
+      "Cool 10 minutes on wire rack.",
+    ]),
+    user_id: "2",
+  },
+  {
+    title: "Tomato and Goat Cheese Dip",
+    image:
+      "https://docs.google.com/drawings/d/e/2PACX-1vQ8ObZwHsFSQkMa6vsMMjWmTbL1t1MMwWvFX_R2_t07KDi0y5ecCNNyioq0b_uPagWZckD6Ji7WTeze/pub?w=962&h=621",
+    ingredients: JSON.stringify([
+      "1 tblspn miced garlic",
+      "1/2 cup olive oil",
+      "2 lbs cherry tomatoes",
+      "4 oz. goat cheese",
+      "1/2 cup fresh basil, slivered",
+    ]),
+    recipe_steps: JSON.stringify([
+      "Steep garlic in olive oil at least 1/2 hour.",
+      "Preheat oven to 400 degrees.",
+      "Wash tomatoes and cut in half or quarters.",
+      "Put tomatoes in a shallow baking dish, pour oil and garlic over and toss lightly.",
+      "Bake 10 minutes.",
+      "Sprinkle chunks of cheese and slivered basil over tomatoes and stir gently.",
+      "Serve over crackers or bread.",
+    ]),
+    user_id: "2",
+  },
 ];
 
 const seedRecipes = () => Recipe.bulkCreate(recipeData);
