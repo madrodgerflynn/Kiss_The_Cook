9c5aa639c3b20da24b8a477fbda2ef9897f1b54d
relero90 <relero90@gmail.com>
2022-09-21 19:11:33 -0600

    added modal pop-up when delete button is clicked

--- public/js/delete-recipe.js
+++ public/js/delete-recipe.js
@@ -1,10 +1,16 @@
-// const deleteRecipe = async () => {
-//   // pop-up modal - confirm that the user really wants to do this
-//   // capture recipe's id value
-//   // template string fetch path with captured id value: /api/delete-recipe/${recipeId}
-//   // fetch request to path
-// };
+const deleteRecipe = async () => {
+  // pop-up modal - confirm that the user really wants to do this
+  // capture recipe's id value
+  let recipeId;
+  // template string fetch path with captured id value: /api/delete-recipe/${recipeId}
+  const path = `/api/delete-recipe/${recipeId}`;
+  // fetch request to path
+  const deleteRes = await fetch(path, () => {});
+  const parse = await deleteRes.json();
+  console.log(parse);
+  console.log("success!");
+};
 
-// if (document.querySelector(".delete")) {
-//   document.querySelector(".delete").addEventListener("click", deleteRecipe);
-// }
+if (document.querySelector(".delete")) {
+  document.querySelector(".delete").addEventListener("click", deleteRecipe);
+}
